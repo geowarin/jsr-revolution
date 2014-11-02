25a078caf05624b4e99af79069f58212399b1154
geowarin <geowarin@gmail.com>
2014-11-02 23:12:57 +0100

    Disable sound by default

--- src/scripts/state/Menu.ts
+++ src/scripts/state/Menu.ts
@@ -8,6 +8,7 @@ module JsrRevolution.State {
       var backColor:Phaser.Graphics = this.add.graphics(0, 0);
       backColor.beginFill(0x000);
       backColor.drawRect(0,0, this.game.width, this.game.height);
+      this.game.sound.mute = window.location.hash !== '#sound';
 
       var background = this.add.sprite(0, 0, 'menu-background');
       this.writerSound = this.add.audio('gun');
